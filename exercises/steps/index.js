73c92fcf4d8a55bce46319c3e0237516ab2e5292
eanorton <edwardnorton@icloud.com>
2018-09-02 14:14:26 -0400

    solution for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
+
+// RECURSIVE SOLUTION: 
+
+// function steps(n, row = 0, stair = '') {
+//   if (n === 0) {
+//     return;
+//   }
+//
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+//
+//   if (star.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//
+//   steps(n, row, stair);
+// }
